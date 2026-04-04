9219b7d15a75b51f1b67cb7d85d408e94354c218
GiovaneLM <giovanelimeiramartins@gmail.com>
2026-04-04 20:26:10 -0300

    lista de exercicio novo

---
+++ logicaProgama/aula5/lista1/exercicio1/index.py
@@ -0,0 +1,3 @@
+'''Exercício 01. Faça um algoritmo que imprima dez vezes o caracter "#" um abaixo do outro. Utilize o comando for.'''
+for i in range(10):
+    print("#")
---
+++ logicaProgama/aula5/lista1/exercicio2/index.py
@@ -0,0 +1,4 @@
+'''Exercício 02.
+Faça um algoritmo que imprima dez vezes o caracter "#" um ao lado do outro. Utilizando o comando for.'''
+for i in range(10):
+    print("#", end="")
