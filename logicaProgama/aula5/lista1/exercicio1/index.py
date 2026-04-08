7b9030b1b893aedaf8637d6e83bc9bffcedea217
GiovaneLM <giovanelimeiramartins@gmail.com>
2026-04-08 10:30:43 -0300

    aula d3e html 06

--- logicaProgama/aula5/lista1/exercicio1/index.py
+++ logicaProgama/aula5/lista1/exercicio1/index.py
@@ -1,3 +1,4 @@
 '''Exercício 01. Faça um algoritmo que imprima dez vezes o caracter "#" um abaixo do outro. Utilize o comando for.'''
-for i in range(10):
-    print("#")
+lista = ["#","#","#","#","#","#","#","#","#","#","#",]
+for i in lista:
+    print(i)
